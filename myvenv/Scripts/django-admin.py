e736997dbcb1c2514810deabf6cb20df73a8fb24
MIROYH <60463288+MIROYH@users.noreply.github.com>
2020-04-17 17:06:55 +0900

    word counter

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\onedrive - 경희대학교\programming\word-counter\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
